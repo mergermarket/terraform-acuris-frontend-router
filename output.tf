d10f0ab951860cf2fd32fd7a32c848bb31b274b9
Marcio Goda <marciogoda@gmail.com>
2021-04-28 11:01:57 +0100

    added missing outputs

--- output.tf
+++ output.tf
@@ -5,3 +5,11 @@ output "alb_dns_name" {
 output "alb_listener_arn" {
   value = module.alb.alb_listener_arn
 }
+
+output "default_target_group_arn" {
+  value = aws_alb_target_group.default_target_group.arn
+}
+
+output "dns_record_fqdn" {
+  value = module.dns_record.fqdn
+}
