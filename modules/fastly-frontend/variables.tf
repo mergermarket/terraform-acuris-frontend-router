c8152dc969ec0d4006842b4dc055742dbbfb3cba
mmgcarlos <carlos.solis@mergermarket.com>
2023-04-05 16:42:01 +0100

    domain no prefix

--- modules/fastly-frontend/variables.tf
+++ modules/fastly-frontend/variables.tf
@@ -10,6 +10,12 @@ variable "additional_domain_names" {
   default     = []
 }
 
+variable "domain_no_prefix" {
+  type        = string
+  description = "Domain without env prefix used for supporting legacy domain urls"
+  default = ""
+}
+
 variable "bare_redirect_domain_name" {
   type        = string
   default     = ""
